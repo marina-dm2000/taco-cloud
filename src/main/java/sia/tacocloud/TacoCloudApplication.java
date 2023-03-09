5df0e5a6f76f95ba8c23aa84d9f77af14cda27aa
marina-dm2000 <86020642+marina-dm2000@users.noreply.github.com>
2023-03-09 11:09:13 +0300

    Start project

---
+++ src/main/java/sia/tacocloud/TacoCloudApplication.java
@@ -0,0 +1,14 @@
+package sia.tacocloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+// Указываем, что это приложение Spring Boot
+@SpringBootApplication
+public class TacoCloudApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TacoCloudApplication.class, args);
+    }
+
+}
