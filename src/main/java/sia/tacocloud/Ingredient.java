b02c48e15dca65307a1bdb0ef882bbc0ff9832ec
marina-dm2000 <marina_dm28@icloud.com>
2023-04-07 16:38:56 +0300

    Работа с Spring Data JPA

--- src/main/java/sia/tacocloud/Ingredient.java
+++ src/main/java/sia/tacocloud/Ingredient.java
@@ -1,6 +1,8 @@
 package sia.tacocloud;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.Id;
 import lombok.*;
 
@@ -10,13 +12,13 @@ import lombok.*;
 @Data
 @Entity
 @AllArgsConstructor // упрощает создание объекта со всеми инициализированными свойствами
-// генерирует приватный конструктор без параметров
-@NoArgsConstructor(access = AccessLevel.PROTECTED, force = true)
-@RequiredArgsConstructor
+// генерирует защищенный конструктор без параметров
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Ingredient {
     @Id
     private String id;
     private String name;
+    @Enumerated(EnumType.STRING)
     private Type type;
 
     public enum Type {
