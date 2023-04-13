27a49f665853b11fafa901493199a6c7a62aec6e
Marina <marina-dm2000@mail.ru>
2023-04-13 14:46:20 +0300

    Откат назад

--- src/main/java/sia/tacocloud/data/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package sia.tacocloud.data;
-
-import org.springframework.data.repository.CrudRepository;
-import sia.tacocloud.User;
-
-public interface UserRepository extends CrudRepository<User, Long> {
-    User findByUsername(String username);
-}
