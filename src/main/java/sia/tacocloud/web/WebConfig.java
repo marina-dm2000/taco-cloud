47f226575391b5d68e95b18bd8bd48cdb4c3a71b
Marina <marina-dm2000@mail.ru>
2023-04-13 14:49:18 +0300

    Откат назад

--- src/main/java/sia/tacocloud/web/WebConfig.java
+++ src/main/java/sia/tacocloud/web/WebConfig.java
@@ -22,6 +22,5 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
         registry.addViewController("/").setViewName("home");
-        registry.addViewController("/login");
     }
 }
